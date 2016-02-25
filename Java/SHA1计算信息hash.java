a8cbb736ace0daaac80f4a0811da18e79c16d7c1
jinhaoplus <rocking2013@hotail.com>
2016-02-25 16:48:38 +0800

    Security Functions

---
+++ Java/SHA1计算信息hash.java
@@ -0,0 +1,25 @@
+//SHA1信息hash
+
+public static String SHA1(String decript) {
+        try {
+            MessageDigest digest = java.security.MessageDigest
+                    .getInstance("SHA-1");
+            digest.update(decript.getBytes());
+            byte messageDigest[] = digest.digest();
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            // 字节数组转换为十六进制数实现信息可视化（非“乱码”，而是规则的16进制字符）
+            for (int i = 0; i < messageDigest.length; i++) {
+                String shaHex = Integer.toHexString(messageDigest[i] & 0xFF);
+                if (shaHex.length() < 2) {
+                    hexString.append(0);
+                }
+                hexString.append(shaHex);
+            }
+            return hexString.toString();
+ 
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
